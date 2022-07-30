3ddae596c6c7c165a2bf579b39d55ff78b4c421d
Lucas Gimenes <lucas_sin5@hotmail.com>
2022-07-29 23:52:13 -0300

    update form login

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -15,33 +15,40 @@ import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 import { MaterialIcons } from "@expo/vector-icons";
 
+const VALID_EMAIL_EXPRESSION =
+  /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+
 export function SignIn() {
   const [isLoading, setIsLoading] = useState(false);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [show, setShow] = useState(false);
 
   const [tooManyRequests, setTooManyRequests] = useState(false);
   const [emailIsInvalid, setEmailIsInvalid] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
   const [passwordIsInvalid, setPasswordIsInvalid] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
   const { colors } = useTheme();
 
   function handleSignIn() {
-    if (!email || !password) {
-      //return Alert.alert("Entrar", "Informe e-mail e senha.");
-      setEmailIsInvalid(true);
-      setPasswordIsInvalid(true);
-      return;
+    if (email.trim() === "") {
+      return setEmailIsInvalid(true);
+    }
+
+    if (!VALID_EMAIL_EXPRESSION.test(email.toLowerCase())) {
+      return setEmailIsInvalid(true);
+    }
+
+    if (password.trim() === "") {
+      return setPasswordIsInvalid(true);
     }
 
     if (password.length < 6) {
       return setPasswordError(true);
     }
 
-    setEmailIsInvalid(false);
-    setPasswordIsInvalid(false);
-    setPasswordError(false);
     setIsLoading(true);
 
     auth()
